d38299b82a252bff989674bc632249280c983953
Anthony <chizzele.oce@gmail.com>
2018-11-06 05:09:01 +1100

    This commit is the current live version at https://www.chizzele.com/Beci_Idea/index.html , assets, and splash screens have been made

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 $('document').ready(function(){
   console.log('ready');
+  const initialWindowSize = $(window).width();
+  validateWindowSize(initialWindowSize);
+  $(window).resize(function(){
+    validateWindowSize($(window).width());
+  });
 });
+
+function validateWindowSize(windowWidth){
+  if (windowWidth > 991){
+    $('.thirdPage').hover(function(){
+      $('.secondPage').hide();
+    });
+    $('.starterPage').hover(function(){
+      $('.secondPage').show();
+    });
+  }else{
+
+  }
+}
